2049f68d6a79d830c86f8cb466b7f43c70c89e75
Shumer-1 <borisov1908vlad@mail.ru>
2024-12-08 17:24:04 +0300

    feat(main): add main class

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,28 @@
 package backend.academy;
 
+import backend.academy.generateFractalFlame.FlameGenerator;
+import backend.academy.generateFractalFlame.MultiThreadFrameGenerator;
+import backend.academy.input.ConsoleInputReader;
+import backend.academy.input.InputReader;
+import backend.academy.session.Session;
+import backend.academy.writer.ConsoleWriter;
+import backend.academy.writer.Writer;
+import java.io.PrintStream;
+import java.util.Random;
+import java.util.Scanner;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        int threadCount = Runtime.getRuntime().availableProcessors();
+        FlameGenerator multiThreading = new MultiThreadFrameGenerator(new Random(System.currentTimeMillis()),
+            threadCount);
+
+        Writer writer = new ConsoleWriter(new PrintStream(System.out));
+        InputReader inputReader = new ConsoleInputReader(new Scanner(System.in));
+        Session session = new Session(multiThreading, writer, inputReader);
+
+        session.startSession();
     }
 }
